70fd4b48a5a0ebdbe4d549d1c06741b98e92ac55
Fuzzy Fiend <50032576+fuzzyfiend@users.noreply.github.com>
2019-09-11 21:55:27 -0500

    add json lib handle json configuration

--- yiff.py
+++ yiff.py
@@ -15,6 +15,7 @@ Scrape yiff.party for archival purposes
 import os
 import sys
 import argparse
+import json
 from pprint import pprint, pformat
 
 # Third-Party Libraries
@@ -36,7 +37,8 @@ def preinit():
     parser.add_argument('-v', '--verbose', action='count', default=0, help="Print verbose output to the console. Multiple v's increase verbosity")
     parser.add_argument('--debug', action='store_true', help="Toggle debugging output to the console.")
     parser.add_argument('-o', '--output', default='siterip', help="Write all files and directory structure to the following location. Default is '%(default)s'")
-    parser.add_argument('-c', '--caching-dir', default=".ypcache", help="Directory for caching metadata. Default is '%(default)s'")
+    parser.add_argument('-c', '--config', default="config.json", help="A JSON configuration to load. Default is '%(default)s'")
+    parser.add_argument('--caching-dir', default=".ypcache", help="Directory for caching metadata. Default is '%(default)s'")
     parser.add_argument('--flush-cache', action='store_true', help="Toggle to flush the cache prior to running")
     pg_state.args = parser.parse_args()
     #endregion
@@ -45,15 +47,24 @@ def init():
     pg_state = State()
     args = pg_state.args
 
-    pg_state.diskcache = Cache(directory=args.caching_dir)
-    if args.flush_cache:
-        pg_state.diskcache.clear()
-    pg_state.scraper = YPScraper()
-
     pg_state.exec_file = os.path.basename(__file__)
     pg_state.exec_dir = os.path.dirname(__file__)
     pg_state.exec_fpath = os.path.abspath(__file__)
     pg_state.output_dir = os.path.join(pg_state.exec_dir, args.output)
+    if args.verbose > 1:
+        crit = ["exec_file", "exec_dir", "exec_fpath", "output_dir"]
+        vLogPGVars(crit, pg_state)
+    os.chdir(pg_state.exec_dir)
+
+    if args.config:
+        print("[*] Loading configuration file: %s" % args.config)
+        j = pg_state.config = json.load(open(args.config))
+        pg_state.patreon_dict = j["patreon"]
+
+    pg_state.diskcache = Cache(directory=args.caching_dir)
+    if args.flush_cache:
+        pg_state.diskcache.clear()
+    pg_state.scraper = YPScraper()
         
 def main():
     ## PreInit program state and handle arguments
@@ -71,11 +82,12 @@ def main():
         if not os.path.exists(output_path):
             os.makedirs(output_path)
         os.chdir(output_path)
+        if args.verbose:
+            print('[*] set pg_state.output_path = %s' % (pg_state.output_path) )
+            print('[*] --> This directory will be created if absent and navigated into')
 
-        # use the following for testing purposes
-        # 20645128 has relatively few submissions
-        # 881729 has quite a few submissions and saved files that span multiple pages
-        patreon_list = pg_state.patreon_list = [20645128, 881792]
+        #[55578,2479000,96435,9608858,706437,126683,881792,175584,460110,777164,3571295,253549,106008,10404964,690415,215736,3708137,2285596,121401,16943278,140212,2937211,10329385,7216191]
+        #patreon_list = pg_state.patreon_list = [55578,2479000,96435,9608858,706437,126683,881792,175584,460110,777164,3571295,253549,106008,10404964,690415,215736,3708137,2285596,121401,16943278,140212,2937211,10329385,7216191]
 
         # foreach patreon
         for pid in patreon_list:
