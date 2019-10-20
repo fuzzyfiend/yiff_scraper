372c132c8d385c07fa448a70cdcf686140ff9eb9
Fuzzy Fiend <50032576+fuzzyfiend@users.noreply.github.com>
2019-10-20 16:03:05 -0500

    add generic request. shouldn't recieve 504 now... but only testing will tell

--- classes/Scraper.py
+++ classes/Scraper.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 ## Standard Libraries
 import os
 import sys
+import time
 from pprint import pprint
 
 ## Third-Party
@@ -71,11 +72,41 @@ class Scraper(BaseObject):
         session.mount('https://', adapter)
         return session
 
+    def doRequest(self, method, url, params=None, stream=False):
+        if method not in self.supported_methods:
+            raise ValueError("(%s) not in (%s)" % (method, self.supported_methods))
+
         try:
-            resp = self.lastRequestResponse = self.req.head(url)
+            if params == None:
+                resp = self.lastRequestResponse = self.req.request(method=method, url=url, stream=stream)
+            else:
+                resp = self.lastRequestResponse = self.req.request(method=method, url=url, params=params, stream=stream)
             resp.raise_for_status()
             return resp
-        except:
+        except Timeout as e:
+            pprint(e)
+            raise
+        except ConnectionError as e:
+            pprint(e)
+            raise
+        except HTTPError as e:
+            if e.response.status_code == 404:
+                #Sometimes we get 404 from yiff.party, can't do anything about it
+                print("[!] Caught %d for url (%s); ignoring" %(e.response.status_code, e.response.url))
+                return None
+            elif e.response.status_code == 504:
+                #Gateway timeout; sleep for a bit 
+                s=15
+                print("[!] Caught %d for url (%s); snoozing for %d seconds" %(e.response.status_code, e.response.url, s))
+                time.sleep(s)
+                return None
+            else:
+                raise
+        except RequestException as e:
+            pprint(e)
+            raise
+        except Exception as e:
+            pprint(e)
             raise
 
     def doHEADRequest(self, url):
